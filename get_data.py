3f94d1ca4b5cfb728ba9dcda22a38b95de87db30
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2019-10-19 17:38:46 -0300

    few changes

--- get_data.py
+++ get_data.py
@@ -1,11 +1,9 @@
 import pandas
 import csv
 import numpy as np
-#LAData = pandas.read_csv('data/LosAngeles.csv')
-#print(type(LAData))
-#LAData.get_value(1, 'averagingperiod')
-#print(LAData.get_values())
-#print(LAData[0][2])
+import matplotlib.pyplot as plt
+import datetime
+import matplotlib.dates as date
 dict = []
 with open('data/LosAngeles.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -35,8 +33,10 @@ with open('data/LosAngeles.csv') as csv_file:
             line_count += 1
     print(f'Processed {line_count} lines.')
 
-LAList_co = [float(item['value']) for item in dict if item['parameter'] == 'co']
-LAList_no2 = [float(item['value']) for item in dict if item['parameter'] == 'no2']
+LAList_co = [float(item['value']) for item in dict if item['parameter'] == 'co' and item['location'] == 'Anaheim']
+LAList_time = [item['date'] for item in dict if item['parameter'] == 'co' and item['location'] == 'Anaheim']
+#LAList_no2 = [float(item['value']) for item in dict if item['parameter'] == 'no2']
+'''
 LAList_o3 = [float(item['value']) for item in dict if item['parameter'] == 'o3']
 LAList_so2 = [float(item['value']) for item in dict if item['parameter'] == 'so2']
 LAList_pm10 = [float(item['value']) for item in dict if item['parameter'] == 'pm10']
@@ -47,3 +47,23 @@ print("Quantidade de o3:" + str(sum(LAList_o3)/len(LAList_o3)))
 print("Quantidade de so2:" + str(sum(LAList_so2)/len(LAList_so2)))
 print("Quantidade de pm10:" + str(sum(LAList_pm10)/len(LAList_pm10)))
 print("Quantidade de pm25:" + str(sum(LAList_pm25)/len(LAList_pm25)))
+'''
+print(LAList_co)
+aloha = []
+for i in range(0, len(LAList_co)):
+    aloha.append(i)
+plt.plot(aloha, LAList_co)
+plt.xlabel('CO em ppm')
+plt.ylabel('Data')
+plt.show()
+'''
+LAList_better_time = []
+for time in LAList_time:
+    time_split = time.split(',')
+    LAList_better_time.append(time_split[0][5:24])  
+print(LAList_better_time)
+dates = date.date2num(LAList_better_time)
+plt.plot_date(LAList_better_time, LAList_co, tz="US/Eastern", xdate=True)
+plt.xlabel('CO em ppm')
+plt.ylabel('Data')
+plt.show()'''
