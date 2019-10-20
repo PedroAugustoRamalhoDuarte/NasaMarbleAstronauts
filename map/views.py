45fd18ef67daa99281de03b2889d5a03e7e9e3be
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2019-10-19 21:08:16 -0300

    Better post

--- map/views.py
+++ map/views.py
@@ -11,7 +11,17 @@ def index(request):
 
 
 def get_data(request):
-    return HttpResponse("Hello, you're at the sensors4G index!")
+    template_name = 'arduino.html'
+    context = {}
+    if request.method == 'POST':
+        data = request.POST.get('a0')
+        print(data)
+        context['a0'] = data
+    else:
+        context['a0'] = 'irmão manda um POST ae'
+
+
+    return render(request, template_name, context)
 
 @csrf_exempt
 def sensor_log(request):
