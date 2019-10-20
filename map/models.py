4c0a1305052e092a5a7d73bbcd375cb6e32077ba
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2019-10-20 10:22:24 -0300

    print request

--- map/models.py
+++ map/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Arduino(models.Model):
+
+    name = models.CharField(max_length=5)
+    data = models.IntegerField
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- map/views.py
+++ map/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
-
+from .models import Arduino
 # Create your views here.
 
 def index(request):
@@ -14,13 +13,16 @@ def get_data(request):
     template_name = 'arduino.html'
     context = {}
     if request.method == 'POST':
-        print(request.POST)
-        data = request.POST.get('a0')
-        print(data)
-        context['a0'] = data
+        a0 = request.POST.get('a0')
+        a1 = request.POST.get('a1')
+        a2 = request.POST.get('a2')
+        context['a0'] = a0
+        context['a1'] = a1
+        context['a2'] = a2
+
     else:
         context['a0'] = 'irmão manda um POST ae'
-
+        context['data'] = Arduino.objects.all()
 
     return render(request, template_name, context)
 
